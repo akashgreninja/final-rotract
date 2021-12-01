fe92672bc5809f6c2dbd630bf732e9bdf5c737e1
Akash Uday <84433561+akashgreninja@users.noreply.github.com>
2021-12-01 18:56:19 +0530

    Delete lib directory

--- lib/Register/fake_ui.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui' as ui;
-
-// ignore: camel_case_types
-class platformViewRegistry {
-  static registerViewFactory(String viewId, dynamic cb) {
-    ui.platformViewRegistry.registerViewFactory(viewId, cb);
-  }
-}
--- lib/event.dart
+++
@@ -1,9 +0,0 @@
-class Event_detail {
-  final String pic;
-  final String name;
-  final DateTime dateTime;
-  final String category;
-  final String description;
-
-  Event_detail(this.pic, this.name, this.dateTime, this.category, this.description);
-}
